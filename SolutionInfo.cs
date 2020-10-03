a8c999fa34d8002dc0ad3a3082327a168b9e3815
Chuck Hill <chuckhill2@gmail.com>
2020-10-02 23:00:47 -0700

    Add common SolutionInfo assembly properties to synchronize across projects.

---
+++ SolutionInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+[assembly: AssemblyCompany("Chuck Hill")]
+[assembly: AssemblyCopyright("Copyright © 2018 Chuck Hill")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("2.2.2.0")]
