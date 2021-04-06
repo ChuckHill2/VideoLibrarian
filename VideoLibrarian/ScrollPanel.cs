99ad14000b41e8eeaac97c200824dc072a4eb7f5
Chuck Hill <chuckhill2@gmail.com>
2021-04-06 14:14:14 -0700

    Add copyright headers

--- VideoLibrarian/ScrollPanel.cs
+++ VideoLibrarian/ScrollPanel.cs
@@ -1,3 +1,32 @@
+//--------------------------------------------------------------------------
+// <summary>
+//   
+// </summary>
+// <copyright file="ScrollPanel.cs" company="Chuck Hill">
+// Copyright (c) 2020 Chuck Hill.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2.1
+// of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// The GNU Lesser General Public License can be viewed at
+// http://www.opensource.org/licenses/lgpl-license.php. If
+// you unfamiliar with this license or have questions about
+// it, here is an http://www.gnu.org/licenses/gpl-faq.html.
+//
+// All code and executables are provided "as is" with no warranty
+// either express or implied. The author accepts no liability for
+// any damage or loss of business that this product may cause.
+// </copyright>
+// <repository>https://github.com/ChuckHill2/VideoLibrarian</repository>
+// <author>Chuck Hill</author>
+//--------------------------------------------------------------------------
 //#undef DEBUG
 
 using System;
