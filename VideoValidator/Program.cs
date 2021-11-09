2cfcc3292ce2d4451927abaf7f3d87012f731746
Chuck Hill <chuckhill2@gmail.com>
2021-11-08 16:57:55 -0800

    VideoValidator: Need assembly resolver for embedded assemblies referenced by VideoLibrarian.MovieProperties.VerifyVideoFile().

--- VideoValidator/Program.cs
+++ VideoValidator/Program.cs
@@ -34,6 +34,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using VideoLibrarian;
 
@@ -47,6 +48,7 @@ namespace VideoValidator
 
         static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve; //Load embedded assemblies
             Log.MessageCapture += (Severity sev, string msg) => Console.WriteLine($"{sev}: {msg}");
             ParseCommandLine(args);  //populate MediaFolders from the command-line
 
@@ -199,6 +201,59 @@ files there are (6hrs or more for >2000 videos). This is due to the necessity
 of having to read the entire content of all the large video files.
 ");
             Environment.Exit(1);
+        }
+
+        /// <summary>
+        /// VideoLibrarian.MovieProperties.VerifyVideoFile() may call embedded assembly for retrieving
+        /// properties from within the video file, so we need a mechanism for extracting it upon demand.
+        /// </summary>
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            //This function can ONLY use THIS assembly or assemblies from the GAC because if this routine or attendant
+            //subroutines needs to use an assembly that needs to be "found" then recursion will occur!
+            AssemblyName asmName = new AssemblyName(args.Name);
+            Exception ex = null;
+            Assembly asm = null;
+
+            if (asmName.Name.EndsWith(".resources", StringComparison.OrdinalIgnoreCase)) return null;
+            if (asmName.Name.EndsWith(".XmlSerializers", StringComparison.OrdinalIgnoreCase)) return null;
+
+            try
+            {
+                var loadedAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+                asm = loadedAssemblies.FirstOrDefault(a => (a.IsDynamic ? false : string.Compare(Path.GetFileNameWithoutExtension(a.Location), asmName.Name, true) == 0));
+                if (asm != null) return asm;
+
+                //Dig deeper. Look for embedded assembly resource only within OUR assemblies.
+                var dllname = $".{asmName.Name}.dll";
+                foreach (Assembly a in loadedAssemblies)
+                {
+                    if (a.IsDynamic) continue;
+                    if (a.Location.ContainsI("Microsoft.NET")) continue; //Exclude microsoft .net assemblies
+
+                    var resname = a.GetManifestResourceNames().FirstOrDefault(m => m.EndsWith(dllname, StringComparison.InvariantCultureIgnoreCase));
+                    if (resname == null) continue;
+
+                    var fullpath = Path.Combine(Path.GetDirectoryName(a.Location), dllname);
+                    using (var fs = File.OpenWrite(fullpath)) a.GetManifestResourceStream(resname).CopyTo(fs);
+                    asm = Assembly.LoadFrom(fullpath);
+                    break;
+                }
+            }
+            catch (Exception e) { ex = e; }
+            finally
+            {
+                if (asm == null)
+                {
+                    Log.Write(Severity.Error, "Unresolved assembly: \"{0}\"", args.Name);
+                    if (ex != null) Log.Write(Severity.Error, "Assembly Resolver Error: \"{0}\"", ex);
+                }
+                else
+                {
+                    Log.Write(Severity.Success, "Resolved assembly: \"{0}\"", asm.IsDynamic ? asm.ToString() : asm.Location);
+                }
+            }
+            return asm;
         }
     }
 }
