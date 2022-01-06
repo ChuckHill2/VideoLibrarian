9a563c5a99363f5e315ad0cbbf53e90571d12f5d
Chuck Hill <chuckhill2@gmail.com>
2022-01-05 20:45:01 -0800

    Added code for caching performance test.

--- VideoLibrarian/ResourceCache.cs
+++ VideoLibrarian/ResourceCache.cs
@@ -27,6 +27,7 @@
 // <repository>https://github.com/ChuckHill2/VideoLibrarian</repository>
 // <author>Chuck Hill</author>
 //--------------------------------------------------------------------------
+#define PERFORMANCE
 using System.Drawing;
 
 namespace VideoLibrarian
@@ -43,101 +44,141 @@ namespace VideoLibrarian
         private static Bitmap _tileBackground = null;
         public static Bitmap TileBackground 
         {
+        #if (!PERFORMANCE)
+            get => global::VideoLibrarian.Properties.Resources.DetailedTileBackground;
+        #else
             get
             {
                 if (_tileBackground == null) _tileBackground = global::VideoLibrarian.Properties.Resources.DetailedTileBackground;
                 return _tileBackground;
             }
+        #endif
         }
 
         private static Bitmap _imdbIcon = null;
         public static Bitmap ImdbIcon
         {
+        #if (!PERFORMANCE)
+            get => global::VideoLibrarian.Properties.Resources.IMDB_Logo_2016;
+        #else
             get
             {
                 if (_imdbIcon == null) _imdbIcon = global::VideoLibrarian.Properties.Resources.IMDB_Logo_2016;
                 return _imdbIcon;
             }
+        #endif
         }
 
         private static Bitmap _imdbIconHover = null;
         public static Bitmap ImdbIconHover
         {
+        #if (!PERFORMANCE)
+            get => global::VideoLibrarian.Properties.Resources.IMDB_Logo_2016_Hover;
+        #else
             get
             {
                 if (_imdbIconHover == null) _imdbIconHover = global::VideoLibrarian.Properties.Resources.IMDB_Logo_2016_Hover;
                 return _imdbIconHover;
             }
+        #endif
         }
 
         private static Bitmap _ratingsStar = null;
         public static Bitmap RatingsStar
         {
+        #if (!PERFORMANCE)
+            get => global::VideoLibrarian.Properties.Resources.RatingsStar;
+        #else
             get
             {
                 if (_ratingsStar == null) _ratingsStar = global::VideoLibrarian.Properties.Resources.RatingsStar;
                 return _ratingsStar;
             }
+        #endif
         }
 
         private static Bitmap _checkboxChecked = null;
         public static Bitmap CheckboxChecked
         {
-            get
+        #if (!PERFORMANCE)
+            get => global::VideoLibrarian.Properties.Resources.CheckboxChecked;
+        #else
+           get
             {
                 if (_checkboxChecked == null) _checkboxChecked = global::VideoLibrarian.Properties.Resources.CheckboxChecked;
                 return _checkboxChecked;
             }
+        #endif
         }
 
         private static Bitmap _checkboxUnchecked = null;
         public static Bitmap CheckboxUnchecked
         {
-            get
+        #if (!PERFORMANCE)
+            get => global::VideoLibrarian.Properties.Resources.CheckboxUnchecked;
+        #else
+           get
             {
                 if (_checkboxUnchecked == null) _checkboxUnchecked = global::VideoLibrarian.Properties.Resources.CheckboxUnchecked;
                 return _checkboxUnchecked;
             }
+        #endif
         }
 
         public static Font _fontRegular = null;
         public static Font FontRegular
         {
+        #if (!PERFORMANCE)
+            get => new Font("Lucida Sans", 9F, FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+        #else
             get
             {
                 if (_fontRegular == null) _fontRegular = new Font("Lucida Sans", 9F, FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
                 return _fontRegular;
             }
+        #endif
         }
 
         public static Font _fontMedium = null;
         public static Font FontMedium
         {
+        #if (!PERFORMANCE)
+            get => new Font("Lucida Sans", 12F, FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+        #else
             get
             {
                 if (_fontMedium == null) _fontMedium = new Font("Lucida Sans", 12F, FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
                 return _fontMedium;
             }
+        #endif
         }
 
         public static Font _fontBold = null;
         public static Font FontBold
         {
+        #if (!PERFORMANCE)
+            get => _fontBold = new Font("Lucida Sans", 9F, FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+        #else
             get
             {
                 if (_fontBold == null) _fontBold = new Font("Lucida Sans", 9F, FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
                 return _fontBold;
             }
+        #endif
         }
 
         public static Font _fontLargeBold = null;
         public static Font FontLargeBold
         {
+        #if (!PERFORMANCE)
+            get => new Font("Lucida Sans", 14F, FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+        #else
             get
             {
                 if (_fontLargeBold == null) _fontLargeBold = new Font("Lucida Sans", 14F, FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
                 return _fontLargeBold;
             }
+        #endif
         }
     }
 }
