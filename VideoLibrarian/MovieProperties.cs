a143bd65c879478a1b555fad4bcf993f702655f4
Chuck Hill <chuckhill2@gmail.com>
2024-08-24 22:05:27 -0700

    Web scrape of "Summary" field has changed.

--- VideoLibrarian/MovieProperties.cs
+++ VideoLibrarian/MovieProperties.cs
@@ -545,7 +545,8 @@ namespace VideoLibrarian
                     summaryhtml = RegexCache.RegEx(@"<a .+?href='/name[^>]+>(?<NAME>[^<]+)</a>", RE_options).Replace(summaryhtml,m=>" "+m.Groups["NAME"].Value);
 
                     //Find all the different summaries. They may have entities and/or newlines (e.g. "<br/>") embedded.
-                    mc = RegexCache.RegEx(@"<div class='ipc-html-content ipc-html-content--base[^']*' role='presentation'>\s*<div class='ipc-html-content-inner-div'>(?:<div[^>]+>)*(?<SUMMARY>.+?)(?:</div>|<span)", RE_options).Matches(summaryhtml);
+                    //mc = RegexCache.RegEx(@"<div class='ipc-html-content ipc-html-content--base[^']*' role='presentation'>\s*<div class='ipc-html-content-inner-div'>(?:<div[^>]+>)*(?<SUMMARY>.+?)(?:</div>|<span)", RE_options).Matches(summaryhtml);
+                    mc = RegexCache.RegEx(@"<div class='ipc-html-content ipc-html-content--base' role='presentation'><div class='ipc-html-content-inner-div' role='presentation'>(?<SUMMARY>.+?)(?:</div>|<span)", RE_options).Matches(summaryhtml);
                     if (mc.Count > 0)
                     {
                         var summaries = mc.Cast<Match>().Select(m => m.Groups["SUMMARY"].Value).Append(this.Summary??string.Empty);
